53e8535874f65f7f2cd08bca3c5621a229e20465
honeydev <lebedev.alexey@protonmail.com>
2023-10-16 23:58:09 +1000

    Add extract session from localStorage.

--- ui/src/Session.elm
+++ ui/src/Session.elm
@@ -1,5 +1,6 @@
 module Session exposing (..)
 
+import Json.Decode as Decode
 import Json.Encode as Encode
 import Ports
 
@@ -14,12 +15,38 @@ type alias SessionData =
     }
 
 
+sessionEncoder : { a | token : String } -> Encode.Value
 sessionEncoder sessionData =
     Encode.object
         [ ( "token", Encode.string sessionData.token )
         ]
 
 
+sessionDecoder : Decode.Decoder SessionData
+sessionDecoder =
+    Decode.map SessionData (Decode.field "token" Decode.string)
+
+
+extractSession : Maybe String -> Session
+extractSession rawSession =
+    case rawSession of
+        Just v ->
+            case Decode.decodeString sessionDecoder v of
+                Ok sessionData ->
+                    Active sessionData
+
+                Err err ->
+                    let
+                        _ = Debug.toString err 
+                            |> Debug.log "Cant parse session data:"
+                    in
+                    Unauthenticated
+
+        Nothing ->
+            Unauthenticated
+
+
+storeSession : SessionData -> Cmd msg
 storeSession sessionData =
     sessionEncoder sessionData
         |> Encode.encode 0
